7169ce860540095cb38370d9feeed43d03352d86
Anthony <aquinoanthony851@gmail.com>
2024-09-28 15:14:03 -0600

    shuffle grind list and adding shaping list

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
-const randomizeBtn = document.querySelector('.randomize-btn');
+const randomizeBtn = document.querySelector('.randomize-btn')
+const grindList = [];
+const cubeList = [];
 
-randomizeBtn.addEventListener('click', () => {
+document.addEventListener('DOMContentLoaded', () => {
   fetch('./db.json')
-    .then(res => res.json())
-    .then(data => console.log(data.employees))
-  
-})
+  .then(res => res.json())
+  .then(data => {
+    const employees = data.employees;
+
+    employees.forEach(employee => {
+      
+      if (employee.cube) {
+        cubeList.push(employee.first_name)
+      }
+      // create an algorithm to rotate cube operators and use splice() or ...rest method to add trailing operator to grinding.
+    });
+  });
+
+  randomizeBtn.addEventListener('click', () => {
+    const shuffledGrinding = shuffleArray([...grindList]);
+    const operatorCells = document.querySelectorAll('.operator-name');
+
+    operatorCells.forEach((cell, index) => {
+      if (shuffledGrinding[index]) {
+        cell.textContent = shuffledGrinding[index];
+      }
+    })
+  });
+
+});
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
